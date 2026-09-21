3fa976df52aecde894e9e7ebd4a1b3e60d9d5974
mfauzidr <mfdr.fauzi97@gmail.com>
2026-09-21 22:04:56 +0700

    feat: setup todo structure and basic todo rendering

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,33 @@
 import "./style.css";
+import { todoData } from "./data/todo.data";
+
+const todos = [...todoData];
+
+function renderTodos() {
+  const todoList = document.querySelector<HTMLUListElement>("#todo-list");
+  const doneList = document.querySelector<HTMLUListElement>("#done-list");
+
+  if (!todoList || !doneList) {
+    throw new Error("Todo list element not found");
+  }
+
+  todoList.innerHTML = "";
+  doneList.innerHTML = "";
+
+  const todoItems = todos.filter((todo) => !todo.completed);
+  const doneItems = todos.filter((todo) => todo.completed);
+
+  todoItems.forEach((todo) => {
+    const li = document.createElement("li");
+    li.textContent = todo.title;
+    todoList.appendChild(li);
+  });
+
+  doneItems.forEach((todo) => {
+    const li = document.createElement("li");
+    li.textContent = todo.title;
+    doneList.appendChild(li);
+  });
+}
+
+renderTodos();
